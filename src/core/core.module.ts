14986151d21dac78b0691ec00ac52a0d9112715e
Bahi Habash <bahi.habash.edu@gmail.com>
2025-11-06 08:40:12 +0200

    FIX import LoggerModue from local-module not the package (they export the same module name)

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,7 +1,7 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
-import { LoggerModule } from 'nestjs-pino';
+import { LoggerModule } from './logger/logger.module';
 
 /**
  * Aggregates and globally provides core application modules (e.g., config, database, logger).
