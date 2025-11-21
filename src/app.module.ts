f2e6f9f1f649d8fb3ce762cecb91862f2702fb56
Bahi Habash <bahi.habash.edu@gmail.com>
2025-11-21 20:26:56 +0200

    FEAT: Initial setup modules for Analytics, Application, Auth, Club, Invitation, Member, Team, and User modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ClubModule } from './modules/club/club.module';
+import { TeamModule } from './modules/team/team.module';
+import { MemberModule } from './modules/member/member.module';
+import { AnalyticsModule } from './modules/analytics/analytics.module';
+import { InvitationModule } from './modules/invitation/invitation.module';
+import { ApplicationModule } from './modules/application/application.module';
 
 /**
  * The root module of the application and the starting point.
@@ -8,6 +15,16 @@ import { UserModule } from './user/user.module';
  * It imports all other.
  */
 @Module({
-  imports: [CoreModule, UserModule],
+  imports: [
+    CoreModule,
+    UserModule,
+    AuthModule,
+    ClubModule,
+    TeamModule,
+    MemberModule,
+    AnalyticsModule,
+    InvitationModule,
+    ApplicationModule,
+  ],
 })
 export class AppModule {}
