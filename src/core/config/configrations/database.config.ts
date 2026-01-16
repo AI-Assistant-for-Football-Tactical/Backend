9d7bb3e27d07b51fc90104370099719f1bf25eab
Bahi Habash <bahi.habash.edu@gmail.com>
2026-01-16 14:10:20 +0200

    refactor(config): add JSDocs and use AppEnv enum

--- src/core/config/configrations/database.config.ts
+++ src/core/config/configrations/database.config.ts
@@ -5,6 +5,11 @@ import { ConfigService } from '@nestjs/config';
 export class DatabaseConfig {
   constructor(private configService: ConfigService) {}
 
+  /**
+   * Returns the database connection URL.
+   *
+   * @returns The database connection URL.
+   */
   get url(): string {
     return this.configService.get<string>('DATABASE_URL')!;
   }
